56bd21a01aa29cab1d67bd0f6c5376f690ad1c7a
nmitra <neel1@e.ntu.edu.sg>
2013-09-02 22:43:48 +0800

    Revert "Modifications in Transaction class"
    This reverts commit 529958db07bb1b10bd0677862f1381d5b9a22c6a.

--- src/Transaction.java
+++ src/Transaction.java
@@ -6,19 +6,14 @@ import java.util.GregorianCalendar;
 
 public class Transaction {
 
-
-        private Buyer buyer;
-        private Seller seller;
-	private Product product;
-        //private int buyer;         NEEL COMMENTED
-	//private int seller;        NEEL COMMENTED
-	//private int product;       NEEL COMMENTED
+	private int buyer;
+	private int seller;
+	private int product;
 	private String time;
 	private double amountPaid;
 	private double rating;
-
-
-	/*public Transaction(int buyer, int seller, int product, double amountPaid){
+	
+	public Transaction(int buyer, int seller, int product, double amountPaid){
 		this.buyer = buyer;
 		this.seller = seller;
 		this.product = product;
@@ -28,20 +23,6 @@ public class Transaction {
     	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
     	time = sdf.format(cal.getTime());
 	}
-         */   //NEEL commented
-
-        public Transaction(Agent agent,Product product,double amountPaid){
-            if(agent instanceof Buyer)
-               this.buyer = (Buyer)agent;
-            if(agent instanceof Seller)
-                this.seller = (Seller)agent;
-            this.product = product;
-            this.amountPaid = amountPaid;
-    	Calendar cal = Calendar.getInstance();
-    	cal.getTime();
-    	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-    	time = sdf.format(cal.getTime());
-        }
 	public void create(){
 
 	}
@@ -55,22 +36,13 @@ public class Transaction {
 		return t;
 	}
 
-//	public int getBuyer() {           NEEL COMMENTED
+	public int getBuyer() {
+		return buyer;
+	}
 
-//		return buyer;
-//	}
-
-        public Buyer getBuyer(){
-            return buyer;
-        }
-
-//	public int getSeller() {        NEEL COMMENTED
-//		return seller;
-//	}
-
-        public Seller getSeller(){
-            return seller;
-        }
+	public int getSeller() {
+		return seller;
+	}
 
 	public String getTime() {
 		return time;
