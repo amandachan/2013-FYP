a64a6c5ba2f56359faeecdb9009ec9d16b4d05ee
amandachan <chanamanda@AMANDAchans-MacBook-Pro.local>
2013-09-16 11:19:13 +0800

    Shi Qing's part on Central Authority

--- src/main/CentralAuthority.java
+++ src/main/CentralAuthority.java
@@ -1,128 +1,262 @@
 package main;
-import agent.Buyer;
-import agent.Seller;
-
+ 
+import java.text.DecimalFormat;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+ 
+import environment.Environment;
+ 
 public class CentralAuthority {
-
-	private environment environmentType;
-	
-	//attack and defense name
-	private String defenseType;
-	private String attackType;	
-	private buyerList<Buyer> buyers;
-	private sellerList<Seller> sellers;
-
-	//sellers sales, reputation, reputation difference per day
-	private int[][] dailySales;
-	private double[][] dailyRep;
-	private double[][] dailyRepDiff;
-	
-	//Defense model cost time per day
-	private double[] defenseTime_day;
-	private double[][] dailyAvgWeights;
-	private double[][] dailyMCC;
-	
-	 public void setUpEnvironment(String attackName, String defenseName){
-		 
-		 if(Parameter.includeICLUB(defenseName)){			
-				if (Parameter.RATING_TYPE.equalsIgnoreCase("binary")) {
-					environmentType = new eCommerceB(attackName, defenseName);
-				} else if (Parameter.RATING_TYPE.equalsIgnoreCase("multinominal")) {
-					environmentType = new eCommerceM(attackName, defenseName);
-				} else{
-					environmentType = new eCommerceM(attackName, defenseName);
-				}
-			} else if(Parameter.includeWMA(m_defenseName)){
-				environmentType = new eCommerceR(attackName, defenseName);
-			} else if(Parameter.includeEA(m_defenseName)){
-				if (Parameter.RATING_TYPE.equalsIgnoreCase("binary")) {
-					environmentType = new eCommerceB(attackName, defenseName);
-				} else if (Parameter.RATING_TYPE.equalsIgnoreCase("multinominal")) {
-					environmentType = new eCommerceM(attackName, defenseName);
-				} else if (Parameter.RATING_TYPE.equalsIgnoreCase("real")) {
-					environmentType = new eCommerceR(attackName, defenseName);
-				} 	
-			} else{
-				//for binary ratings, nondefense, BRS, iCLUB, TRAVOS, personalized,
-				environmentType = new eCommerceB(attackName, defenseName);
-			}
-		 	environmentType.generateInstances();
-		 	
-		 	
-		 	//agent setting
-		 	int numBuyers = Parameter.NO_OF_DISHONEST_BUYERS + Parameter.NO_OF_HONEST_BUYERS;
-			int numSellers = environmentType.getNumberSellers(); 
-			for(int i = 0; i < numBuyers; i++){
-				int bid = i;
-				if(bid < Parameter.NO_OF_DISHONEST_BUYERS || bid >= numBuyers){
-					buyers.add(i,new Buyer(bid, false, attackName, environmentType));
-				} else{
-					buyers.add(i, new Buyer(bid, true, defenseName, environmentType));
-				}
-				
-			}
-			for(int k = 0; k < numSellers; k++){
-				int sid = k;
-				if(sid < Parameter.NO_OF_DISHONEST_SELLERS){
-					sellers.add(k,new Seller(sid, false, environmentType));
-				} else{
-					sellers.add(k,new Seller(sid, true, environmentType));
-				}			
-			}
-			
-			//set up the global information for buyers;
-			for(int i = 0; i < numBuyers; i++){			
-				buyers.get(i).setGlobalInformation(buyers, sellers);
-			}
-			
-			//for the evolutionary algorithm
-			if(Parameter.includeEA(defenseType) || Parameter.includeWMA(defenseType)){
-				int tnType = 1;
-				if(defenseType.equalsIgnoreCase("ea0")){
-					tnType = 0;
-				} else if(defenseType.equalsIgnoreCase("ea1")){
-					tnType = 1;
-				} else if(defenseType.equalsIgnoreCase("ea2")){
-					tnType = 2;
-				} 
-				for(int i = 0; i < numBuyers; i++){			
-					buyers.get(i).InitialTrustNetwork(tnType);
-				}
-				for(int i = 0; i < numBuyers; i++){		
-					int day = 0;
-					buyers.get(i).resetWitness(day);
-				}
-			}		
-		 
-	 }
-	 
-	 public void simulateEnvironment(){
-		 
-	 }
-	 
-	 public void evaluateDefenses(){
-		 
-	 }
-	 
-	 public void showOutput(){
-		 
-	 }
-	 
-	 public void exportToDB(){
-		 
-	 }
-	 
-	 public void importFromDB(){
-		 
-	 }
-	 
-	 public void getCentralReputation(){
-		 
-	 }
-	 
-	 public void displaySuggestions(){
-		 
-	 }
-	 
-	 
+ 
+    private Environment env;
+    private String m_defenseName;
+    private String m_attackName;
+    private ArrayList<Buyer> m_buyers;
+    private ArrayList<Seller> m_sellers
+     
+    //sellers sales, reputation, reputation difference per day
+    private int[][] m_dailySales;
+    private double[][] m_dailyRep;
+    private double[][] m_dailyRepDiff;
+    //Defense model cost time per day
+    private double[] defenseTime_day;
+    private double[][] m_dailyAvgWeights;
+    private double[][] m_dailyMCC;
+ 
+    public CentralAuthority(){          
+            m_dailySales = new int[Parameter.NO_OF_DAYS + 1][Parameter.NO_OF_DISHONEST_SELLERS + Parameter.NO_OF_HONEST_SELLERS];
+            m_dailyRep  = new double[Parameter.NO_OF_DAYS + 1][2];
+            m_dailyMCC= new double[Parameter.NO_OF_DAYS + 1][2];
+            m_dailyRepDiff  = new double[Parameter.NO_OF_DAYS + 1][2];
+            defenseTime_day = new double[Parameter.NO_OF_DAYS + 1];
+            m_dailyAvgWeights = new double[Parameter.NO_OF_DAYS + 1][2];
+    }   
+         
+      
+    public void setUpEnvironment(String attackName, String defenseName){
+        env.createEnvironment(attackName, defenseName);
+        env.agentSetting(attackName, defenseName);
+    }
+      
+    public ArrayList simulateEnvironment(String attackName, String defenseName, boolean dailyPrint) throws ClassNotFoundException, NoSuchMethodException, SecurityException{            
+             
+        //step 1: initialize initialize the e commerce
+        m_attackName = new String(attackName);
+        m_defenseName = new String(defenseName);
+        setUpEnvironment(attackName, defenseName);      
+        ArrayList transList = new ArrayList();
+        //TODO discuss about the name specification
+        m_buyers=env.getBuyerList();
+        m_sellers=env.getSellerList();
+         
+        //Day 0 is the eve of Day 1 (only for agents' decision making), transactions start on Day 1             
+        for (int day = 0; day <= Parameter.NO_OF_DAYS; day++){                                                   
+                     
+            if (day != 0){
+                for (int count = 0; count<m_buyers.size();count++){
+                    m_buyers.get(count).setCredits(m_buyers.get(count).getCredits()+Parameter.CREDITS_PER_TURN);
+                }
+            }
+                     
+            for (int a = 0; a<PseudoRandom.randInt(1, Parameter.transaction_limit);a++){
+                DecimalFormat roundoff = new DecimalFormat("#.##");
+                Transaction trans = new Transaction(day);               
+                double cost = trans.getProductQty()*trans.getPrice();
+                cost = Double.valueOf(roundoff.format(cost));
+                if (cost>m_buyers.get(trans.getBuyerID()).getCredits()){
+                    trans.setRemarks("Transaction Failed: Buyer not enough Credit");
+                } else {
+                    trans.setRemarks("Transaction Successful");
+                    m_buyers.get(trans.getBuyerID()).setCredits(m_buyers.get(trans.getBuyerID()).getCredits()-cost);
+                }
+                transList.add(trans);
+            }
+                     
+            //step 2: Attack model (dishonest buyers)               
+            attack(day);            
+                     
+            //step 3: Defense model (honest buyers)
+            long defensetimeStart = new Date().getTime();
+            defense(day);   
+            long defensetimeEnd = new Date().getTime();
+             
+            //TODO how to store transaction
+            defenseTime_day[day] = (-defensetimeStart + defensetimeEnd) / 1000.0;               
+            m_dailySales = m_eCommerce.getDailySales(); 
+            m_dailyRep[day] = m_eCommerce.getDailyReputation();
+            m_dailyRepDiff[day] = m_eCommerce.getDailyReputationDiff();
+            m_dailyMCC[day] = m_eCommerce.getDailyMCC();
+            env.setDay(day); //update to next day
+            avgerWeights(day);
+            if(dailyPrint){
+                //print the transactions for different sellers
+                System.out.print("Day " + day + ": ");                  
+                System.out.print("   |sellers' transactions|: ");
+                for (int i = 0; i < Parameter.NO_OF_HONEST_SELLERS   + Parameter.NO_OF_DISHONEST_SELLERS; i++) {
+                    if(i == Parameter.NO_OF_DISHONEST_SELLERS){
+                        System.out.print(", ");
+                    }                       
+                    System.out.print(" " + m_dailySales[day][i]);                   
+                }       
+                System.out.print("   |rep MAE|: " + m_dailyRep[day][0] + "  " + m_dailyRep[day][1]);
+                System.out.print("   |repDiff MAE|: " + m_dailyRepDiff[day][0] + "  " + m_dailyRepDiff[day][1]);
+                System.out.println(" avgWeights: " + m_dailyAvgWeights[day][0] + "  " + m_dailyAvgWeights[day][1]);
+            }               
+        }
+     
+        return transList;
+    }
+     
+    private void attack(int day){
+         
+        int numBuyers = Parameter.NO_OF_DISHONEST_BUYERS + Parameter.NO_OF_HONEST_BUYERS;
+        //Attack model (dishonest buyers), give rating/ perform attack              
+        for(int i = 0; i < numBuyers; i++){
+            int bid = i;
+            if(m_buyers.get(bid).getIsHonest() == false){
+                m_buyers.get(bid).giveRating(day);
+            }
+        }
+        for(int i = 0; i < numBuyers; i++){
+            int bid = i;
+            if(m_buyers.get(bid).getIsHonest() == false){
+                m_buyers.get(bid).perform_model(day);
+            }
+        }
+    }
+     
+    private void defense(int day){
+         
+        int numBuyers = Parameter.NO_OF_DISHONEST_BUYERS + Parameter.NO_OF_HONEST_BUYERS;
+        //Attack model (dishonest buyers), give rating/ perform defense             
+        for(int i = 0; i < numBuyers; i++){
+            int bid = i;
+            if(m_buyers.get(bid).getIsHonest() == true){
+                m_buyers.get(bid).giveRating(day);
+            }
+        }
+        for(int i = 0; i < numBuyers; i++){
+            int bid = i;
+            if(m_buyers.get(bid).getIsHonest() == true){
+                m_buyers.get(bid).perform_model(day);
+            }
+        }
+    }
+     
+    private void avgerWeights(int day){
+         
+        int db = Parameter.NO_OF_DISHONEST_BUYERS;
+        int hb = Parameter.NO_OF_HONEST_BUYERS; 
+        if(Parameter.includeWhitewashing()){
+            db = db + (day) * db;
+        }       
+         
+        //these code for trust models: trustworthiness for local/partial advisors;
+        if(Parameter.includeWMA(m_defenseName) || Parameter.includeEA(m_defenseName)){
+            int numDA = 0; //number of dishonest advisors;
+            int numHA = 0; //number of honest advisors;
+            for(int i = Parameter.NO_OF_DISHONEST_BUYERS; i < Parameter.NO_OF_DISHONEST_BUYERS + hb; i++){
+                int bid = i;
+                ArrayList<> weights_BA;
+                weights_BA.add(m_buyers.get(bid).getTrusts());
+                ArrayList <Buyer> advisors = m_buyers.get(bid).getAdvisors();             
+                for(int j = 0; j < advisors.size(); j++){                    
+                    int aid = advisors.get(j);
+                    if(aid == bid)continue;
+                    if(aid < Parameter.NO_OF_DISHONEST_BUYERS || aid >= Parameter.NO_OF_DISHONEST_BUYERS + hb){
+                        m_dailyAvgWeights[day][0] += weights_BA.get(aid);
+                        numDA++;
+                    } else{
+                        m_dailyAvgWeights[day][1] += weights_BA.get(aid);
+                        numHA++;
+                    }
+                }               
+            }
+            if (numDA != 0) {
+                m_dailyAvgWeights[day][0] /= (numDA);
+            }           
+            if (numHA != 0) {
+                m_dailyAvgWeights[day][1] /= (numHA);       
+            }
+        }
+        else{
+            //these code for trust models: trustworthiness for all advisors;
+            int numDA = 0;
+            int numHA = 0;
+            for(int i = Parameter.NO_OF_DISHONEST_BUYERS; i < Parameter.NO_OF_DISHONEST_BUYERS + hb; i++){
+                int bid = i;                
+                for (int k = 0; k < 2; k++) {                
+                    int sid = Parameter.TARGET_DISHONEST_SELLER;
+                    if (k == 1)sid = Parameter.TARGET_HONEST_SELLER;
+                    ArrayList <> SaverTA;
+                    SaverTA.add(m_buyers.get(bid).getAverageTrusts(sid));
+                    if(SaverTA.get(0) >= 0){//dishonest advisors
+                        m_dailyAvgWeights[day][0] += SaverTA[0];
+                        numDA++;
+                    }
+                    if(SaverTA.get(1) >= 0){//dishonest advisors
+                        m_dailyAvgWeights[day][1] += SaverTA[1];
+                        numHA++;
+                    }
+                }   
+            }       
+ 
+            if (numDA != 0) {
+                m_dailyAvgWeights[day][0] /= (numDA);
+            }           
+            if (numHA != 0) {
+                m_dailyAvgWeights[day][1] /= (numHA);       
+            }   
+        }       
+         
+    }
+     
+    public void evaluateDefenses(ArrayList<String> defenseNames, ArrayList<String> attackNames, String evaluateName) throws ClassNotFoundException, NoSuchMethodException, SecurityException{
+         
+        int runtimes = Parameter.NO_OF_RUNTIMES;                    //runtimes =  50
+         
+        //output the result: [|transactions|, time]
+        double[][][][] results = new double[runtimes][defenseNames.length][attackNames.length][2];
+        for(int i = 0; i < runtimes; i++){
+            for(int j = 0; j < defenseNames.size(); j++){            
+                for(int k = 0; k < attackNames.size(); k++){                 
+                    System.err.print("  runtimes = " + i + ",   defense = " + defenseNames[j] + ",   attack = " + attackNames[k]);
+                                     
+                    long start = new Date().getTime();
+                     // [true / false] means print/not daily result     
+                    transList = simulateEnvironment(attackNames.get(k), defenseNames.get(j), false); 
+                    long end = new Date().getTime();
+                    System.err.println("   time =  " + (end - start)/1000.0 + " s");
+                    //TODO ADD LATER: results[i][j][k] = generate_report(i); //report include the run count                 
+                }
+            }           
+        }
+     
+        //TODO ADD LATER: getStatistic(results, defenseNames, attackNames, evaluateName);
+ 
+    }
+ 
+      
+     public void showOutput(){
+          
+     }
+      
+     public void exportToDB(){
+          
+     }
+      
+     public void importFromDB(){
+          
+     }
+      
+     public void getCentralReputation(){
+          
+     }
+      
+     public void displaySuggestions(){
+          
+     }
+      
+      
 }
